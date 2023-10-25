f924f2726b78fd812fbc86bcde4f6add18e63853
davidededominicis <83349571+davidededominicis@users.noreply.github.com>
2023-10-25 12:14:04 +0200

    Update SparseMatrix.cpp

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -137,8 +137,60 @@ void SparseMatrixCOO::PrintMatrix(){
             std::cout<<""<<std::endl;
             cols_value.clear();
         }
+
+SparseMatrixCSR SparseMatrix::switch_toCSR(){
+    std::vector<int> rows_coordinates;
+
+        rows_idx.resize(values.size());
+        unsigned int count=0, index=0, diff;
+
+        for(unsigned int i=0; i<rows_idx.size(); i++){
+            if(rows_idx[i]!=0 && rows_idx[i]>rows_idx[i-1]){
+                diff=rows_idx[i]-rows_idx[i-1];
+                for(unsigned int j=0; j<diff; j++){
+                    rows_coordinates[index]=i-1;
+                    index++;
+                }
+            }
+        }
+
+        for(int i=0; i<rows_coordinates.size(); i++){
+            std::cout<<rows_coordinates[i]<<"\t";
+        }
+
+        return SparseMatrixCSR(rows_coordinates, values, columns);
+};
     };
 
+SparseMatrixCOO SparseMatrix::switchtoCOO(){
+    std::vector<int> row_idx;
+        //I need the number of the rows and the size of number of non-zero element inside the sparse amtrix
+        const int numRows=this->n_row();
+
+        row_idx.resize(numRows+1);
+        
+        row_idx[0]=0;
+        unsigned int count=0;
+
+        for(unsigned int i=0; i<size; i++){
+            for(unsigned int j=0; j<size; j++){
+                if(rows[j]==i){
+                    count++;
+                    row_idx[i+1]=count;
+                }
+                //case in which the row has all zero elements
+                else if(rows[j]!=i){
+                    row_idx[i+1]=count;
+                }
+            }
+        }
+
+        for(int i=0; i<numRows+1; i++){
+            std::cout<<row_idx[i]<<"\t";
+        }
+        std::cout<<std::endl;
+        return SparseMatrixCOO(row_idx, values, columns);
+}
 
 
 SparseMatrixCSR::SparseMatrixCSR(const std::vector <int> input_rows, const std::vector <double> input_values,  const std::vector <int> input_columns): 
