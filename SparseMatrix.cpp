f3548fb88cd38dd7b8a39039d1448699ca295084
BarbaraLol <147236478+BarbaraLol@users.noreply.github.com>
2023-10-28 12:21:16 +0200

    SparseMatrix.cpp

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -9,12 +9,24 @@
 -parentesone
 */
 
-SparseMatrix::SparseMatrix (
-    const std::vector <double> &input_values,  const std::vector <int> &input_columns)  :  values(input_values), 
-    columns(input_columns){};
+//Constructor
+//SparseMatrix::SparseMatrix(const std::vector <double>& input_values,  const std::vector <int>& input_columns)  :  values(input_values), columns(input_columns) {}
+//Destructor for total cleanup
+SparseMatrix::~SparseMatrix(){}
 
-SparseMatrixCOO::SparseMatrixCOO(const std::vector <int> &input_rows, const std::vector <double> &input_values,  const std::vector <int> &input_columns): 
-    SparseMatrix(input_values, input_columns), rows(input_rows){};
+
+SparseMatrixCOO::SparseMatrixCOO(const std::vector <int>& input_rows, const std::vector <double>& input_values,  const std::vector <int>& input_columns): SparseMatrix(input_values, input_columns), rows(input_rows){
+    values = new double[input_values.size()];
+    columns = new int[input_columns.size()];
+    std::copy(input_values.begin(), input_values.end(), values);
+    std::copy(input_columns.begin(), input_columns.end(), columns);
+}
+
+//Destructor to deallocate memory when done
+SparseMatrixCOO::~SparseMatrixCOO(){
+    delete[] values;
+    delete[] columns;
+}
     
 const double SparseMatrixCOO::operator()(const  int n, const  int m) const {
         //throw an error
@@ -35,10 +47,10 @@ const double SparseMatrixCOO::operator()(const  int n, const  int m) const {
             }
         }
         return num;
-    }
+}
 
 // Writing operator.
-double &SparseMatrixCOO::operator()(const  int n, const  int m) {
+double &SparseMatrixCOO::operator()(const  int n, const  int m){
         int max_row=this->n_row();
         int max_col=this->n_col();
         if (n>max_row || m>max_col){
@@ -57,29 +69,28 @@ double &SparseMatrixCOO::operator()(const  int n, const  int m) {
         //if(flag==false){ //Probabilmente questo non serve 
             throw std::invalid_argument("You can only modify the non-zero elements of the sparse matrix");
         //}
-        
-    }
+}
 
-//Matrix-vector product
-std::vector<double> SparseMatrixCOO::operator *(const std::vector<double>& x) {
-        if (x.size() != n_col()) {
+// Matrix-vector product
+std::vector<double> SparseMatrixCOO::operator *(const std::vector<double>& x){
+        if (x.size() != n_col()){
             throw std::invalid_argument("Incompatible dimension ");
             return std::vector<double>();
         }
 
         std::vector<double> y(n_row(), 0.0);
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++){
             y[rows[i]] += values[i] * x[columns[i]];
         }
 
-        for (unsigned int i =0; i<n_row(); i++) {
+        for (unsigned int i =0; i<n_row(); i++){
             std::cout << y[i] << std::endl;
         }
         return y;
-    };
+}
 
-//matrix printing function
+// Matrix printing function
 void SparseMatrixCOO::PrintMatrix(){
         int i=0, num_rows=n_row(), num_cols=n_col();
         std::vector<int> cols_value;  
@@ -139,143 +150,97 @@ void SparseMatrixCOO::PrintMatrix(){
             std::cout<<""<<std::endl;
             cols_value.clear();
         }
-
-SparseMatrixCSR SparseMatrix::switch_toCSR(){
-    std::vector<int> rows_coordinates;
-
-        rows_idx.resize(values.size());
-        unsigned int count=0, index=0, diff;
-
-        for(unsigned int i=0; i<rows_idx.size(); i++){
-            if(rows_idx[i]!=0 && rows_idx[i]>rows_idx[i-1]){
-                diff=rows_idx[i]-rows_idx[i-1];
-                for(unsigned int j=0; j<diff; j++){
-                    rows_coordinates[index]=i-1;
-                    index++;
-                }
-            }
-        }
-
-        for(int i=0; i<rows_coordinates.size(); i++){
-            std::cout<<rows_coordinates[i]<<"\t";
-        }
-
-        return SparseMatrixCSR(rows_coordinates, values, columns);
-};
-    };
-
-SparseMatrixCOO SparseMatrix::switchtoCOO(){
-    std::vector<int> row_idx;
-        //I need the number of the rows and the size of number of non-zero element inside the sparse amtrix
-        const int numRows=this->n_row();
-
-        row_idx.resize(numRows+1);
-        
-        row_idx[0]=0;
-        unsigned int count=0;
-
-        for(unsigned int i=0; i<size; i++){
-            for(unsigned int j=0; j<size; j++){
-                if(rows[j]==i){
-                    count++;
-                    row_idx[i+1]=count;
-                }
-                //case in which the row has all zero elements
-                else if(rows[j]!=i){
-                    row_idx[i+1]=count;
-                }
-            }
-        }
-
-        for(int i=0; i<numRows+1; i++){
-            std::cout<<row_idx[i]<<"\t";
-        }
-        std::cout<<std::endl;
-        return SparseMatrixCOO(row_idx, values, columns);
 }
 
 
-SparseMatrixCSR::SparseMatrixCSR(const std::vector <int> &input_rows, const std::vector <double> &input_values,  const std::vector <int> &input_columns): 
-    SparseMatrix(input_values, input_columns),    rows_idx(input_rows){};
+//Constructor for SparseMatrix CSR
+SparseMatrixCSR::SparseMatrixCSR(const std::vector <int>& input_rows, const std::vector <double>& input_values,  const std::vector <int>& input_columns): SparseMatrix(input_values, input_columns), rows_idx(input_rows){
+    values = new double[input_values.size()];
+    columns = new int[input_columns.size()];
+    std::copy(input_values.begin(), input_values.end(), values);
+    std::copy(input_columns.begin(), input_columns.end(), columns);
+}
+
+//Destructor to deallocate memory when done
+SparseMatrixCSR::~SparseMatrixCSR() {
+    delete[] values;
+    delete[] columns;
+}
 
     
 unsigned int SparseMatrixCSR::n_row()const{
         return rows_idx.size()-1; 
-        }
+}
 
 void SparseMatrixCSR::PrintMatrix() {
-        int numRows = n_row(); 
-        if (numRows <= 0) {
-            //if  the matrix is empty
-            std::cout << "Sparse matrix is empty" << std::endl;
-            return;
-        }
-
-        int numCols = n_col();
-        for (int row = 0; row < numRows; ++row) {
-            for (int col = 0; col < numCols; ++col) {
-                int index = -1;
-                // Trova l'indice in cui si trova il valore nella colonna corrente
-                for (int i = rows_idx[row]; i < rows_idx[row + 1]; ++i) {
-                    if (columns[i] == col) {
-                        index = i;
-                       // break;
-
-                    }
-                }
-                if (index != -1) {
-                    // Valore non nullo nella colonna
-                    std::cout << values[index] << "\t";
-                } else {
-                    // Valore nullo
-                    std::cout << "0\t";
+    int numRows = n_row(); 
+    if (numRows <= 0) {
+        //if  the matrix is empty
+        std::cout << "Sparse matrix is empty" << std::endl;
+        return;
+    }
+    int numCols = n_col();
+    for (int row = 0; row < numRows; row++) {
+        for (int col = 0; col < numCols; col++) {
+            int index = -1;
+            // Trova l'indice in cui si trova il valore nella colonna corrente
+            for (int i = rows_idx[row]; i < rows_idx[row + 1]; ++i) {
+                if (columns[i] == col) {
+                    index = i;
+                    // break;
                 }
             }
-            std::cout << std::endl;
+            if (index != -1) {
+                // Valore non nullo nella colonna
+                std::cout << values[index] << "\t";
+            } else {
+                // Valore nullo
+                std::cout << "0\t";
+            }
         }
+        std::cout << std::endl;
     }
+}
 
-    //reading operator
+//reading operator
 const double SparseMatrixCSR::operator()(const  int n, const  int m) const {
-        int max_row=this->n_row();
-        int max_col=this->n_col();
-        if (n>max_row || m>max_col){
-            throw std::invalid_argument("Index out of bound"); 
-        }
-        
-        //read
-        double num = 0;
-        for (int i = rows_idx[n]; i < rows_idx[n + 1]; ++i) {
-            if (columns[i] == m) {
-                num=values[i];
-            }
-       
-        }  
-        return num;
+    int max_row=this->n_row();
+    int max_col=this->n_col();
+    if (n>max_row || m>max_col){
+        throw std::invalid_argument("Index out of bound"); 
     }
+        
+    //read
+    double num = 0;
+    for (int i = rows_idx[n]; i < rows_idx[n + 1]; ++i) {
+        if (columns[i] == m) {
+            num=values[i];
+        }
+       
+    }  
+    return num;
+}
 
 //writing operator
-double &SparseMatrixCSR::operator()(const  int n, const  int m) {
-        int max_row=this->n_row();
-        int max_col=this->n_col();
-        if (n>max_row || m>max_col){
-            throw std::invalid_argument("Index out of bound"); 
-        }
+double &SparseMatrixCSR::operator()(const  int n, const  int m){
+    int max_row=this->n_row();
+    int max_col=this->n_col();
+    if (n>max_row || m>max_col){
+        throw std::invalid_argument("Index out of bound"); 
+    }
         
-        //read
-        for (int i = rows_idx[n]; i < rows_idx[n + 1]; ++i) {
-            if (columns[i] == m) {
-                return values[i];
-            }
-       
+    //read
+    for (int i = rows_idx[n]; i < rows_idx[n + 1]; ++i) {
+        if (columns[i] == m) {
+            return values[i];
         }
-
-        throw std::invalid_argument("You can only modify the non-zero elements of the sparse matrix");
-        }
+    }
+    throw std::invalid_argument("You can only modify the non-zero elements of the sparse matrix");
+}
 
 //matrix vector
 std::vector <double> SparseMatrixCSR::operator*(const std::vector<double> &x) {
-        const int numRows= this->n_row();
+    const int numRows= this->n_row();
 
         std::vector<double> y;
         for (int row = 0; row < numRows; ++row) { 
@@ -289,6 +254,58 @@ std::vector <double> SparseMatrixCSR::operator*(const std::vector<double> &x) {
         for (unsigned int i =0; i<n_row(); i++) {
             std::cout << y[i] << std::endl;
         };
-        return y;
-        
-    };
+        return y; 
+}
+
+
+// Following, two stand alone function that transform a COO object into a CSR one an vice versa
+
+SparseMatrixCSR* NewCSR_SparseMatrix(const std::vector <int>& input_rowsCOO, const std::vector <double>& input_valuesCOO,  const std::vector <int>& input_columnsCOO){
+    // Perform the conversion and create a CSR object
+    // I need the number of the rows and the size of number of non-zero element inside the sparse matrix
+    // Initialize the rows_idx vector with zeros.
+    auto max = std::max_element(input_rowsCOO.begin(), input_rowsCOO.end());
+    std::vector<int> rows_idx(*max+2, 0); 
+
+    // Count the number of non-zero elements in each row.
+    for (unsigned int i=0; i<input_rowsCOO.size(); i++) {
+        rows_idx[input_rowsCOO[i] + 1]++;
+    }
+
+    // Calculate the cumulative sum to complete the rows_idx vector.
+    for (int i=1; i<=input_rowsCOO.size(); i++) {
+        rows_idx[i] += rows_idx[i - 1];
+    }
+
+    for(int i=0; i<rows_idx.size(); i++){
+        std::cout<<rows_idx[i]<<"\t";
+    }
+    std::cout<<std::endl;
+
+// Creation of a new CSR object by calling it's constructor
+    SparseMatrixCSR* new_csr = new SparseMatrixCSR(rows_idx, input_valuesCOO, input_columnsCOO);
+    return new_csr;
+}
+
+SparseMatrixCOO* NewCOO_SparseMatrix(const std::vector <int>& input_rowsCSR, const std::vector <double>& input_valuesCSR,  const std::vector <int>& input_columnsCSR){
+    // Perform the conversion and create a COO object
+    // Starting from creating a COO rows vector from a CSR one
+    std::vector<int> rows;
+
+    rows.resize(input_columnsCSR.size());
+    unsigned int count=0, index=0, diff;
+
+    for(unsigned int i=0; i<input_rowsCSR.size(); i++){
+        if(input_rowsCSR[i]!=0 && input_rowsCSR[i]>input_rowsCSR[i-1]){
+            diff=input_rowsCSR[i]-input_rowsCSR[i-1];
+            for(unsigned int j=0; j<diff; j++){
+                rows[index]=i-1;
+                index++;
+            }
+        }
+    }
+
+    // Creation of a new COO object by calling it's constructor
+    SparseMatrixCOO* new_coo = new SparseMatrixCOO(rows, input_valuesCSR, input_columnsCSR);
+    return new_coo;
+}
